8fe6717dc71833a837612ea842e2b280af550497
meke <3197801970@qq.com>
2022-01-29 16:04:06 +0800

    save

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, useAttrs } from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
 
-createApp(App).mount('#app')
+console.log("33433");
+
+createApp(App).use(new VueRouter()).mount("#app");
